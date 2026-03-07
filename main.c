e9d1f1b3190f447b13f582241f2c095460a5c518
DyoDlm <baccus.delamancha@gmail.com>
2026-03-07 13:04:32 +0000

    clean

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 char    *get_next_line(int fd);
 size_t	WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
 
-char	*build_argument(char *prompt, char *model)
+static char	*build_argument(char *prompt, char *model)
 {
 	const char	*close_format = "\", \"stream\":false }  ";
 	const char	*json_format  = "{ \"model\":\"";
@@ -22,26 +22,18 @@ char	*build_argument(char *prompt, char *model)
 	
 	if (!argument)
 		return NULL;
-<<<<<<< HEAD
 	argument[argument_len] = '\0';
 	strlcat(argument, json_format, strlen(argument) + strlen(json_format) + 1);
 	strlcat(argument, model, strlen(argument) + strlen(model) + 1);
 	strlcat(argument, json_format2, strlen(argument) + strlen(json_format2) + 1);
 	strlcat(argument, prompt, strlen(argument) + strlen(prompt) + 1);
 	strlcat(argument, close_format, strlen(argument) + strlen(close_format) + 1);
-	//	printf("argument is : %s\n", argument);
-=======
-	argument[argument_len] = 'a';
-	strlcat(argument, json_format, strlen(argument) + strlen(json_format));
-	strlcat(argument, prompt, strlen(argument) + strlen(prompt));
-	strlcat(argument, close_format, strlen(argument) + strlen(close_format));
 	printf("argument is : %s\n", argument);
->>>>>>> refs/remotes/origin/main
 	//	argument = url + json_format + prompt + close_format;	
 	return argument;
 }
 
-char	*exec(const char *url, char *argument)
+static char	*exec(const char *url, char *argument)
 {
 	CURL *curl_handle;
 	CURLcode res;
@@ -89,39 +81,48 @@ static void	clean_up_json(char **s)
 	size_t		to = 0;
 	size_t		iterator = 0;
 
-	printf("Start len : %d\nEnd len : %d\n", strlen(start), strlen(end));
-	for (size_t i = 0; *s[i] && (!from || !to); i++)
+	if (*s)
+		printf("Cleaning : %s\n", *s);
+	printf("Start len : %ld\nEnd len : %ld\nTotal len : %ld\n", 
+			strlen(start), strlen(end), strlen(*s));
+	
+	for (size_t i = 0; (*s)[i] && (!from || !to); i++)
 	{
 		iterator = 0;
+		printf("A");
 		while (!from
-				&& start[iterator] && *s[i + iterator]
-				&& start[iterator] == *s[i + iterator])
+				&& start[iterator] && (*s)[i + iterator]
+				&& start[iterator] == (*s)[i + iterator])
 			iterator++;
+		printf("B");
 		while (from
-				&& end[iterator] && *s[i + iterator]
-				&& end[iterator] == *s[i + iterator])
+				&& end[iterator] && (*s)[i + iterator]
+				&& end[iterator] == (*s)[i + iterator])
 			iterator++;
+		printf("C");
 		switch (iterator) {
 			case 13:
 				from = i;
 				break ;
 			case 9:
-				to = iterator;
+				to = i;
 				break ;
 			default:
 				break ;
 		}
+		printf("D");
 	}
-	printf("extracting from : %d to %d\n", from, to);
+	printf("extracting from : %ld to %ld\n", from, to);
 	cleaned_up = malloc(to - from + 1);
 	if (!cleaned_up)
 		return ;
+	iterator = 0;
 	while (from < to)
-		cleaned_up[iterator++] = *s[from++];
+		cleaned_up[iterator++] = (*s)[from++];
 	cleaned_up[iterator] = 0;
 	free(*s);
-	printf("Cleaned up res 1 : %s\n", cleaned_up);
 	*s = cleaned_up;
+	printf("Cleaned up res 1 : %s\n", cleaned_up);
 }
 
 static void	display(char *s)
@@ -150,19 +151,12 @@ int	main(int ac, char **av)	//	./ask_littletown [prompt] [model]
 		prompt = strdup(av[1]);
 		model = strdup(av[2]);
 	}
-<<<<<<< HEAD
 	curl_argument = build_argument(prompt, model);
 	response = exec(url, curl_argument);
 	clean_up_json(&response);
 	display(response);
 	free(response);
 	//printf("Prompt : %s\n", prompt);
-=======
-	printf("Prompt : %s\n", prompt);
-	curl_argument = build_argument(prompt);
-
-	exec(url, curl_argument);
->>>>>>> refs/remotes/origin/main
 	free(prompt);
 	free(model);
 	if (curl_argument)
